a2a0d4a7c4830e2d5f42552fb074b91afaae8fe9
groenborg <groenborg27@gmail.com>
2013-10-07 13:32:00 +0200

    Startup Version

---
+++ src/dk/kasper/simon/FlowTwo.java
@@ -0,0 +1,19 @@
+
+package dk.kasper.simon;
+
+/**
+ *
+ *      @author Simon & Kasper
+ * 
+ *      Version 0.0.0.0 
+ * 
+ */
+public class FlowTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
