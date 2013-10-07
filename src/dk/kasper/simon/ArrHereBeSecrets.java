ca5053f73679346d329605c1d33efed7f47f72b1
groenborg <groenborg27@gmail.com>
2013-10-07 15:21:31 +0200

    Version 0.1.1.0

---
+++ src/dk/kasper/simon/ArrHereBeSecrets.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.kasper.simon;
+
+/**
+ *
+ * @author Simon
+ */
+public class ArrHereBeSecrets {
+    
+    
+    
+    public ArrHereBeSecrets(String name){
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    public void dreadPirate(){
+    
+    }
+    
+    
+    public void walkThePlank(){
+    
+    }
+    
+    
+    public void shiverMeTimber(){
+    
+    }
+    
+    
+    public void ohayThar(){
+    
+    }
+    
+    
+    
+    
+}
---
+++ src/dk/kasper/simon/Person.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.kasper.simon;
+
+/**
+ *
+ * @author Simon
+ */
+public class Person {
+ 
+    private String name;
+    private int admin, analyst, creative, finisher;
+    
+    
+    public Person(String name, int admin,int analyst, int creative,int finisher){
+        this.admin = admin;
+        this.analyst = analyst;
+        this.creative = creative;
+        this.finisher = finisher;
+        this.name = name;   
+    }
+    
+    
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.name);
+        return sb.toString();
+    }
+    
+}
