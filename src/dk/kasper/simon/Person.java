909a4720a7f29d1a0d4abee277d9a2e7c3ac7e5d
groenborg <groenborg27@gmail.com>
2013-10-09 13:04:26 +0200

    Version 0.4.6.1

--- src/dk/kasper/simon/Person.java
+++ src/dk/kasper/simon/Person.java
@@ -9,40 +9,20 @@ package dk.kasper.simon;
  * @author Simon
  */
 public class Person {
- 
+
     private String name, secretTitle;
     private int admin, analyst, creative, finisher;
-    
-    
-    public Person(String name, int admin,int analyst, int creative,int finisher){
+
+    public Person(String name, int admin, int analyst, int creative, int finisher) {
         this.admin = admin;
         this.analyst = analyst;
         this.creative = creative;
         this.finisher = finisher;
-        this.name = name;   
-    }
-    public void setName(String name) {
         this.name = name;
     }
 
     public void setSecretTitle(String secretTitle) {
         this.secretTitle = secretTitle;
-    }
-
-    public void setAdmin(int admin) {
-        this.admin = admin;
-    }
-
-    public void setAnalyst(int analyst) {
-        this.analyst = analyst;
-    }
-
-    public void setCreative(int creative) {
-        this.creative = creative;
-    }
-
-    public void setFinisher(int finisher) {
-        this.finisher = finisher;
     }
 
     public int getAdmin() {
@@ -60,7 +40,7 @@ public class Person {
     public int getFinisher() {
         return finisher;
     }
-    
+
     public String showPerson() {
         StringBuilder sb = new StringBuilder();
         sb.append("Name: \t");
@@ -75,14 +55,14 @@ public class Person {
         sb.append(finisher);
         return sb.toString();
     }
-    
-    public String toFile(){
-        String person = name+","+admin+","+analyst+","+creative+","+finisher;
+
+    public String toFile() {
+        String person = name + "," + admin + "," + analyst + "," + creative + "," + finisher;
         return person;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(this.name);
         return sb.toString();
